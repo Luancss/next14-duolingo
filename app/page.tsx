bfdfa7b0054ab43ba636b2cba32b8b4090b9da9b
Luancss <ventobo123@gmail.com>
2024-03-18 16:32:36 -0300

    feat: Add variant and size configurations for buttons

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,19 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex  p-4 space-y-3 flex-col max-w-[200px]">
       <Button size="lg">Olá</Button>
+      <Button size="lg" variant="primary">Olá</Button>
+      <Button size="lg" variant="primaryOutline">Olá</Button>
+      <Button size="lg" variant="secondary">Olá</Button>
+      <Button size="lg" variant="secondaryOutline">Olá</Button>
+      <Button size="lg" variant="danger">Olá</Button>
+      <Button size="lg" variant="dangerOutline">Olá</Button>
+      <Button size="lg" variant="super">Olá</Button>
+      <Button size="lg" variant="superOutline">Olá</Button>
+      <Button size="lg" variant="ghost">Olá</Button>
+      <Button size="lg" variant="sidebar">Olá</Button>
+      <Button size="lg" variant="sidebarOutline">Olá</Button>
     </div>
   )
 }
