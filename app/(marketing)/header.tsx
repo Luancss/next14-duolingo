ae6155735caac409bb0dc2687da8ede49ebb1797
Luancss <ventobo123@gmail.com>
2024-03-18 18:15:23 -0300

    Adding clerk

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,11 @@
 export const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-      Header
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full bg-red-50">
+        <div className="ot">
+
+        </div>
+      </div>
     </header>
   )
 }
