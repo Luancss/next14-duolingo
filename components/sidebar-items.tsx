643884165473c9a6a9fc4d963353597325355270
Luancss <ventobo123@gmail.com>
2024-03-20 18:26:30 -0300

    Sidebar complete

--- components/sidebar-items.tsx
+++ components/sidebar-items.tsx
@@ -1,17 +1,36 @@
-"use client"
+"use client";
 
+import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 type Props = {
   label: string;
   iconSrc: string;
   href: string;
-}
+};
 
 export const SidebarItem = ({ label, iconSrc, href }: Props) => {
+  const pathname = usePathname();
+  const active = pathname === href;
+
   return (
-    <Button>
-      {label}
+    <Button
+      variant={active ? "sidebarOutline" : "sidebar"}
+      className="justify-start h-[52px]"
+      asChild
+    >
+      <Link href={href}>
+      <Image
+        src={iconSrc}
+        alt={label}
+        className="mr-5"
+        height={32}
+        width={32}
+      />
+        {label}
+        </Link>
     </Button>
-  )
-}
+  );
+};
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,9 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+
+import { ClerkLoading, ClerkLoaded, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+
 import Link from "next/link";
 import { SidebarItem } from "./sidebar-items";
 
@@ -11,20 +15,37 @@ export const Sidebar = ({ className }: Props = {}) => {
   return (
     <div
       className={cn(
-        "flex  h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
         className
       )}
     >
       <Link href="/learn">
-      <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-        <Image src="/mascot.svg" alt="Mascot" width={40} height={40} />
-        <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
-          Lingo
-        </h1>
-      </div>
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" alt="Mascot" width={40} height={40} />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
       </Link>
       <div className="flex flex-col gap-y-2 flex-1">
-        <SidebarItem/>
+        <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg" />
+        <SidebarItem
+          label="Leaderboard"
+          href="/leaderboard"
+          iconSrc="/leaderboard.svg"
+        />
+        <SidebarItem label="Quests" href="/quests" iconSrc="/quests.svg" />
+        <SidebarItem label="Shop" href="/shop" iconSrc="/shop.svg" />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton
+            afterSignOutUrl="/"
+          />
+        </ClerkLoaded>
       </div>
     </div>
   );
