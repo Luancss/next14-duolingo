b8610521028a91ee1a9637dc813952238ce1fd91
Luancss <ventobo123@gmail.com>
2024-03-21 17:27:38 -0300

    Create a feed wrapper component

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,13 +1,15 @@
-import { StickyWrapper } from "@/components/sticky-wrapper"
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
 
 const page = () => {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
-      <StickyWrapper>
-        My sticky sidebar
-        </StickyWrapper>
+      <StickyWrapper>My sticky sidebar</StickyWrapper>
+      <FeedWrapper>
+        <div className="h-[200px] w-full bg-blue-500"/>
+      </FeedWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
---
+++ components/feed-wrapper.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  children: React.ReactNode
+};
+
+export const FeedWrapper = ({ children }: Props) => {
+  return (
+    <div className="flex-1 relative top0 pb-10">
+      {children}
+    </div>
+  )
+}
