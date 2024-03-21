506565f781c1ea8dfdae3940c370764de08393ed
Luancss <ventobo123@gmail.com>
2024-03-21 17:22:14 -0300

    Create a sticky component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,12 +8,10 @@ type Props = {
 const MainLayout = ({ children }: Props) => {
   return (
     <>
-    <MobileHeader/>
-    <Sidebar className="hidden lg:flex"/>
+      <MobileHeader />
+      <Sidebar className="hidden lg:flex" />
       <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-        <div className="bg-red-500 h-full">
-        {children}
-        </div>
+        <div className="max-w-[1056px] mx-auto pt-6 h-full">{children}</div>
       </main>
     </>
   );
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,11 @@
+import { StickyWrapper } from "@/components/sticky-wrapper"
+
 const page = () => {
   return (
-    <div>
-      Learn page
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        My sticky sidebar
+        </StickyWrapper>
     </div>
   )
 }
