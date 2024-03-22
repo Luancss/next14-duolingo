466a5aafb1281fbcb09e5a947d2def9988d3c6fc
Luancss <ventobo123@gmail.com>
2024-03-22 18:43:48 -0300

    User progress component

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,11 +1,19 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
 
 const LearnPage = () => {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
-      <StickyWrapper>My sticky sidebar</StickyWrapper>
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{}}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
       <FeedWrapper>
         <Header title="Spanish"/>
       </FeedWrapper>
---
+++ components/user-progress.tsx
@@ -0,0 +1,20 @@
+type Props = {
+  activeCourse: any;
+  hearts: number;
+  points: number;
+  hasActiveSubscription: boolean;
+}
+
+export const UserProgress = ({
+  activeCourse,
+  hearts,
+  points,
+  hasActiveSubscription
+}: Props) => {
+  return (
+    <div>
+
+  
+    </div>
+  )
+}
