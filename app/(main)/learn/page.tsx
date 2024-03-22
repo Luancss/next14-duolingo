c183ff696fffc1fa8e3093c2e6d1d12e7b94123b
Luancss <ventobo123@gmail.com>
2024-03-22 18:40:45 -0300

    Add header component with courses button

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,15 +1,16 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
 
-const page = () => {
+const LearnPage = () => {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>My sticky sidebar</StickyWrapper>
       <FeedWrapper>
-        <div className="h-[200px] w-full bg-blue-500"/>
+        <Header title="Spanish"/>
       </FeedWrapper>
     </div>
   );
 };
 
-export default page;
+export default LearnPage;
