483fe7237c45bead59c309d85f0b48f550f3a5fc
Luancss <ventobo123@gmail.com>
2024-03-22 18:59:50 -0300

    Learn page wrappers complete

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -8,7 +8,7 @@ const LearnPage = () => {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{}}
+          activeCourse={{title: "Spanish", imageSrc: "/es.svg"}}
           hearts={5}
           points={100}
           hasActiveSubscription={false}
--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { InfinityIcon } from "lucide-react";
+
 type Props = {
-  activeCourse: any;
+  activeCourse: {imageSrc: string, title: string};
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
@@ -12,9 +17,44 @@ export const UserProgress = ({
   hasActiveSubscription
 }: Props) => {
   return (
-    <div>
-
-  
+    <div className="flex items-center justify-between gap-x-2 w-full">
+      <Link href="courses">
+        <Button variant="ghost">
+          <Image
+            width={32}
+            height={32}
+            src={activeCourse.imageSrc}
+            alt={activeCourse.title}
+            className="rounded-md border"
+          />
+        </Button>
+      </Link>
+      <Link href="/shop">
+      <Button variant="ghost" className="text-orange-500">
+        <Image
+           src="/points.svg"
+           width={28}
+           height={28}
+           alt="Points"
+           className="mr-2"
+        />
+        {points}
+      </Button>
+      </Link>
+      <Link href="/shop">
+      <Button variant="ghost" className="text-rose-500">
+        <Image
+           src="/heart.svg"
+           width={22}
+           height={22}
+           alt="Hearts"
+           className="mr-2"
+        />
+        {hasActiveSubscription 
+          ? <InfinityIcon className="h-4 w-4 stroke-[3]"/> 
+          : hearts}
+      </Button>
+      </Link>
     </div>
   )
 }
