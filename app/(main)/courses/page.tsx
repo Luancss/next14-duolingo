ef3d3f51fa727a766699428b358cdf114d27ae2a
Luancss <ventobo123@gmail.com>
2024-03-27 16:55:18 -0300

    Creating the course list component

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,16 +1,15 @@
-import { getCourses } from "@/db/queries"
+import { getCourses } from "@/db/queries";
+import { List } from "./list";
 
 const CoursesPage = async () => {
   const data = await getCourses();
 
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
-      <h1 className="text-2xl font-bold text-neutral-700">
-          Language Courses
-      </h1>
-      <List courses={data}/>
-      </div>
-  )
-}
+      <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
+      <List courses={data} activeCourseId={1}/>
+    </div>
+  );
+};
 
-export default CoursesPage
+export default CoursesPage;
