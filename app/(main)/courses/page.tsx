eaad4784b0918317177f0fc890ffd2eaa8428b72
Luancss <ventobo123@gmail.com>
2024-03-27 16:45:05 -0300

    Get all courses from the database

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,9 +1,14 @@
-const CoursesPage = () => {
+import { getCourses } from "@/db/queries"
+
+const CoursesPage = async () => {
+  const data = await getCourses();
+
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">
           Language Courses
       </h1>
+      <List courses={data}/>
       </div>
   )
 }
