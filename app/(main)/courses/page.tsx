eda4105e534fc41f5d8f52edaae4a8747dcd1c2a
Luancss <ventobo123@gmail.com>
2024-03-27 16:40:38 -0300

    Create a learn page

---
+++ app/(main)/courses/page.tsx
@@ -0,0 +1,11 @@
+const CoursesPage = () => {
+  return (
+    <div className="h-full max-w-[912px] px-3 mx-auto">
+      <h1 className="text-2xl font-bold text-neutral-700">
+          Language Courses
+      </h1>
+      </div>
+  )
+}
+
+export default CoursesPage
