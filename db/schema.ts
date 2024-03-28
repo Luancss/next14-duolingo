65e2caae7fcbbf326c699a8d6817cba88082eb00
Luancss <ventobo123@gmail.com>
2024-03-28 18:07:28 -0300

    Add course relations and user progress implementation

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,31 @@
-import { pgTable, serial, text} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   imageSrc: text("image_src").notNull(),
 });
+
+export const coursesReletions = relations(courses, ({many}) => ({
+  userProgress: many(userProgress),
+}))
+
+export const userProgress = pgTable("user_progress", {
+  userId: text("user_id").primaryKey(),
+  userName: text("username").notNull().default("User"),
+  userImageSrc: text("user_text_image_src").notNull().default("/mascot.svg"),
+  activeCourseId: integer("active_course_id").references(() => courses.id, {
+    onDelete: "cascade",
+  }),
+  hearts: integer("hearts").notNull().default(5),
+  points: integer("hearts").notNull().default(0),
+});
+
+
+export const userProgressRelations = relations(userProgress, ({one}) => ({
+  acticeCourse: one(courses, {
+    fields: [userProgress.activeCourseId],
+    references: [courses.id],
+  })
+}))
