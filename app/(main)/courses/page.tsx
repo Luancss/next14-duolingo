3b0fa9aabd50c8559b07e7534d58cc5f39385a86
Luancss <ventobo123@gmail.com>
2024-03-28 18:21:33 -0300

    Functionality to select the course by id

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,13 +1,16 @@
-import { getCourses } from "@/db/queries";
+import { getCourses, getUserProgress } from "@/db/queries";
 import { List } from "./list";
 
 const CoursesPage = async () => {
   const data = await getCourses();
+  const userProgress = await getUserProgress();
 
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
-      <List courses={data} activeCourseId={1}/>
+      <List 
+        courses={data}
+        activeCourseId={userProgress?.activeCourseId ?? null}/>
     </div>
   );
 };
--- db/queries.ts
+++ db/queries.ts
@@ -1,8 +1,28 @@
 import {cache} from "react";
 import db from "@/db/drizzle";
+import { auth } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { userProgress } from "@/db/schema";
+
+export const getUserProgress = cache(async () => {
+  const {userId} = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      acticeCourse: true,
+    }
+  })
+
+  return data;
+})
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
 
-  return data
+  return data;
 })
