7d01d65dbef23062007bd5aa64b1dee84da558bc
Luancss <ventobo123@gmail.com>
2024-04-10 23:28:54 -0300

    Added options for the first challenge in the database

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -78,8 +78,24 @@ const main = async () => {
         challengeId: 1,
         imageSrc: "/man.svg",
         correct: true,
-        text: "El hombre",
+        text: "el hombre",
         audioSrc: "/es_man.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: "/woman.svg",
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: "/robot.svg",
+        correct: false,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
       }
     ]);
 
