a511d4a8f02984c43b3b92c2c47c9cbcb80f8e17
Luancss <ventobo123@gmail.com>
2024-04-22 18:39:48 -0300

    Refactor LessonButton component logic for positioning and icon selection

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -1,4 +1,13 @@
-import React from 'react'
+"use client"
+
+import Link from "next/link";
+import { Check, Crown, Star } from "lucide-react";
+import {CircularProgressbarWithChildren} from "react-circular-progressbar"
+
+import "react-circular-progressbar/dist/styles.css";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 type Props = {
   id: number;
@@ -17,6 +26,31 @@ export const LessonButton= ({
   current,
   percentage
 }: Props) => {
+  const cycleLength = 8;
+  const cycleIndex = index % cycleLength;
+
+  let identationLevel;
+
+  if (cycleIndex <= 2) {
+    identationLevel = cycleIndex
+  } else if (cycleIndex < 4) {
+    identationLevel = 4 - cycleIndex;
+  }  else if (cycleIndex < 6) {
+    identationLevel = 4 - cycleIndex;
+  } else {
+    identationLevel = cycleIndex - 8;
+  }
+
+  const rightPosition = identationLevel * 40;
+
+  const isFirst = index === 0;
+  const isLast = index === totalCount;
+  const isCompleted = !current && !locked;
+
+  const Icon = isCompleted ? Check : isLast ? Crown : Star;
+
+  const href = isCompleted ? `/lesson/${id}` : "/lesson";
+
   return (
     <div>
       {id}
