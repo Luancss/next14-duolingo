d91de1c36506a733cabef220f006217972c903c9
Luancss <ventobo123@gmail.com>
2024-04-24 23:46:43 -0300

    Add CircularProgressbarWithChildren component

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -72,6 +72,33 @@ export const LessonButton = ({
                 className="absolute left-1/2 -bottom-2 w-0 h-0 border-x-8 border-x-transparent border-t-8 transform -translate-x-1/2"
               />
             </div>  
+            <CircularProgressbarWithChildren
+               value={Number.isNaN(percentage) ? 0 : percentage}
+               styles={{
+                 path: {
+                   stroke: "#4ade80",
+                 },
+                 trail: {
+                   stroke: "#e5e7eb",
+                 }
+               }}
+          >
+            <Button
+              size="rounded"
+              variant={locked ? "locked" : "secondary"}
+              className="w-[70px] h-[70px] border-b-8"
+            >
+              <Icon
+               className={cn(
+                 "h-10 w-10",
+                 locked
+                 ? "fill-neutral-400 text-neutral-400 stroke-neutral-400"
+                 : "fill-primary-foreground text-primary-foreground",
+                 isCompleted && "fill-none stroke-[4]"
+               )}
+              />
+            </Button>
+          </CircularProgressbarWithChildren>
           </div>
         ) : (
           <div>
