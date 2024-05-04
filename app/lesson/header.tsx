ef9f83809ffefa31eddad3fc843b359eebcdb0a5
Luancss <ventobo123@gmail.com>
2024-05-04 01:54:23 -0300

    Create a layout of lesson

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -1,3 +1,5 @@
+import { X } from "lucide-react";
+
 type Props = {
   hearts: number;
   percentage: number;
@@ -9,9 +11,12 @@ export const Header = ({
   hasActiveSubscription
 }: Props) => {
   return (
-    <div>
-      
-    </div>
+    <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
+      <X
+        onClick={() => {}}
+        className="text-slate-500 hover:opacity-75 transition cursor-pointer"
+      />
+    </header>
   )
 }
 
