eb9e590f6ebc6d4c4aaae0f59b16423d7665ef66
Luancss <ventobo123@gmail.com>
2024-05-05 20:39:49 -0300

    Implement Quiz Component with Initial State

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -24,6 +24,18 @@ export const Quiz = ({
 }: Props) => {
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex((challenge) => !challenge.completed);
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+
+  const challenge = challenges[activeIndex];
+
+
+  const title = challenge.type === "ASSIST"
+   ? "Select the corret meaning"
+   : challenge.question
 
   return (
     <>
@@ -32,6 +44,18 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="text-lg lg-text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
