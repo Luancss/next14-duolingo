a2b71836e8fd9d0abebe5d6699aca9302157d2ba
Luancss <ventobo123@gmail.com>
2024-05-05 20:18:05 -0300

    Exitmodal component complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 import {ClerkProvider} from "@clerk/nextjs";
 import { Toaster } from "sonner";
 import "./globals.css";
+import { ExitModal } from "@/components/modals/exit-modal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <Toaster/>
+      <ExitModal/>
       <body className={font.className}>{children}</body>
     </html>
     </ClerkProvider>
