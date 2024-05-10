552de7be83cd937be8b9577c6fdba475410cca2f
Luancss <ventobo123@gmail.com>
2024-05-10 00:53:02 -0300

    Add logic to update challenge progress and user progress upon completion

--- actions/challenge-progress.ts
+++ actions/challenge-progress.ts
@@ -2,9 +2,10 @@
 
 import db from "@/db/drizzle";
 import { getUserProgress } from "@/db/queries";
-import { challenges } from "@/db/schema";
+import { challengeProgress, challenges, userProgress } from "@/db/schema";
 import { auth } from "@clerk/nextjs";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 
 export const upsertChallengeProgress = async (challengeId: number) => {
   const {userId} = await auth();
@@ -28,4 +29,34 @@ export const upsertChallengeProgress = async (challengeId: number) => {
   }
 
   const lessonId = challenge.lessonId;
+
+  const existingChallengeProgress = await db.query.challengeProgress.findFirst({
+    where: and(
+      eq(challengeProgress.userId, userId),
+      eq(challengeProgress.challengeId, lessonId)
+    ),
+  });
+
+  const isPractice = !!existingChallengeProgress;
+
+  if (currentUserProgress.hearts === 0 && !isPractice) {
+    return { error: "hearts"}
+  }
+
+  if (isPractice) {
+    await db.update(challengeProgress).set({
+      completed: true,
+    }).where(eq(challengeProgress.id, existingChallengeProgress.id));
+
+    await db.update(userProgress).set({
+      hearts: Math.min(currentUserProgress.hearts + 1, 5),
+      points: currentUserProgress.points + 10,
+    }).where(eq(userProgress.userId, userId));
+
+    revalidatePath("/learn");
+    revalidatePath("/lessons");
+    revalidatePath("/quests");
+    revalidatePath("/leaderboard");
+    revalidatePath(`/lessons/${lessonId}`);
+  }
 }
