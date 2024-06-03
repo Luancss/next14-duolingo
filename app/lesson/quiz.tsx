4e5dd4071bb0fc4ea00aa6d99cce437a03dded22
Luancss <ventobo123@gmail.com>
2024-06-02 22:46:48 -0300

    Add responsive design to completion message

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -10,6 +10,7 @@ import Footer from "./footer";
 import Challenge from "./challenge";
 import { reduceHearts } from "@/actions/user-progress";
 import { useAudio } from "react-use";
+import Image from "next/image";
 
 type Props = {
   initialPercentage: number;
@@ -120,11 +121,36 @@ export const Quiz = ({
     }
   };
 
-  if (!challenge) {
+  if (true || !challenge) {
     return (
-      <div>
-        Finished the challenge!
-      </div>
+      <>
+        <div className="flex flex-col gap-y-4 lg:gap-y-8 max-w-lg mx-auto text-center items-center justify-center h-full">
+          <Image
+            src="/finish.svg"
+            alt="Finish"
+            className="hidden lg:block"
+            height={100}
+            width={100}
+          />
+           <Image
+            src="/finish.svg"
+            alt="Finish"
+            className="block lg:hidden"
+            height={50}
+            width={50}
+          />
+          <h1 className="text-xl lg:text-3xl font-bold text-neutral-700">
+            Great job! <br />
+            You&apos;ve completed the lesson
+          </h1>
+          <div className="flex items-center gap-x-4 w-full">
+            <ResultCard
+              variant="points"
+              value={challenges.length * 10}
+            />
+          </div>
+        </div>
+      </>
     );
   }
 
