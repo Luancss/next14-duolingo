21def1a57fc3c5f6850d3d0142aa6994592f9134
Luancss <ventobo123@gmail.com>
2024-06-10 18:39:29 -0300

    LeaderBoard page component

--- db/queries.ts
+++ db/queries.ts
@@ -205,7 +205,6 @@ export const getLessonPercentage = cache(async () => {
   return percentage;
 });
 
-
 export const getUserSubscription = cache(async () => {
   const { userId } = auth();
 
@@ -225,4 +224,23 @@ export const getUserSubscription = cache(async () => {
     ...data,
     isActive: !!isActive,
   };
+});
+
+export const getTopTenUsers = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) return [];
+
+  const data = await db.query.userProgress.findMany({
+    orderBy: (userProgress, { desc }) => [desc(userProgress.points)],
+    limit: 10,
+    columns: {
+      userId: true,
+      userName: true,
+      userImageSrc: true,
+      points: true,
+    },
+  });
+
+  return data;
 });
