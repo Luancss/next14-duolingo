dae298251f982c4bdd1333b25a3c5a48fb43c51c
Luancss <ventobo123@gmail.com>
2024-06-12 01:07:26 -0300

    Implement unit CRUD operations with admin authorization and React Admin integration

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -1,10 +1,13 @@
 "use client"
 
-import { Admin, ListGuesser, Resource } from "react-admin"
+import { Admin, Resource } from "react-admin"
 import simpleRestProvider from "ra-data-simple-rest"
 import { CourseList } from "./course/list"
 import { CourseCreate } from "./course/create"
 import { CourseEdit } from "./course/edit"
+import { UnitList } from "./unit/list"
+import { UnitCreate } from "./unit/create"
+import { UnitEdit } from "./unit/edit"
 
 const dataProvider = simpleRestProvider("/api")
 
@@ -12,11 +15,18 @@ const App = () => {
   return (
     <Admin dataProvider={dataProvider}>
       <Resource
-        name="courses" 
+        name="courses"
         list={CourseList}
         create={CourseCreate}
         edit={CourseEdit}
-        recordRepresentation="title"    
+        recordRepresentation="title"  
+      />
+      <Resource
+        name="units"
+        list={UnitList}
+        create={UnitCreate}
+        edit={UnitEdit}
+        recordRepresentation="title" 
       />
     </Admin>
   )
