509efa61db77f8222dcc1802c37336ff25311dc0
Luancss <ventobo123@gmail.com>
2024-06-12 00:45:20 -0300

    Implement course CRUD operations with admin authorization

--- app/admin/course/edit.tsx
+++ app/admin/course/edit.tsx
@@ -1,13 +1,13 @@
-import { Create, SimpleForm, TextInput, required } from "react-admin";
+import { Edit, SimpleForm, TextInput, required } from "react-admin";
 
 export const CourseEdit = () => {
   return (
-    <Create>
+    <Edit>
       <SimpleForm>
         <TextInput source="id" validate={[required()]} label="Id" />
         <TextInput source="title" validate={[required()]} label="Title" />
         <TextInput source="ImageSrc" validate={[required()]} label="Image" />
       </SimpleForm>
-    </Create>
+    </Edit>
   );
 };
